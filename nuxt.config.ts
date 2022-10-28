8dbbf3d84cff1514aef20f6ed7d029e0ee1e8036
Vladimir <pestov.web@yandex.ru>
2022-10-28 16:17:45 +1000

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/strapi','@formkit/nuxt','@nuxtjs/tailwindcss'],
-    strapi: {
-        url: process.env.STRAPI_URL || 'http://localhost:1337',
-        prefix: '/api',
-        version: 'v4',
-        cookie: {},
-        cookieName: 'strapi_jwt'
-    }
-})
+  modules: ["@nuxtjs/strapi", "@formkit/nuxt", "@nuxtjs/tailwindcss"],
+  strapi: {
+    url: process.env.API_URL || "http://localhost:1337",
+    prefix: "/api",
+    version: "v4",
+    cookie: {},
+    cookieName: "strapi_jwt",
+  },
+});
