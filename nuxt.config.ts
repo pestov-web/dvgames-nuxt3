c5b6839a9e1853e927d89ec1799362b08bcac635
Vladimir <pestov.web@yandex.ru>
2022-10-30 00:55:52 +1000

    chore: test deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,21 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/strapi", "@formkit/nuxt", "@nuxtjs/tailwindcss"],
   // uncomment before deploy
-  runtimeConfig: {
-    strapi: {
-      // nuxt/strapi options available server-side
-      url: "http://localhost:1337",
-    },
-    public: {
-      strapi: {
-        // nuxt/strapi options available client-side
-        url: "http://apid.pestov-web.ru",
-      },
-    },
-  },
+  // runtimeConfig: {
+  //   strapi: {
+  //     // nuxt/strapi options available server-side
+  //     url: "http://localhost:1337",
+  //   },
+  //   public: {
+  //     strapi: {
+  //       // nuxt/strapi options available client-side
+  //       url: "http://apid.pestov-web.ru",
+  //     },
+  //   },
+  // },
   strapi: {
     // comment before deploy =)
-    // url: process.env.API_URL || "http://localhost:1337",
+    url: process.env.API_URL || "http://localhost:1337",
     prefix: "/api",
     version: "v4",
     cookie: {},
